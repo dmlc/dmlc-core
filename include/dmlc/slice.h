340a72d17d26ffc6b638a474766d504e253eaf53
muli <muli@cs.cmu.edu>
2015-04-06 00:33:53 -0400

    tiny fix

--- include/dmlc/slice.h
+++ include/dmlc/slice.h
@@ -1,4 +1,4 @@
-/**
+/*!
  * \file   slice.h
  * \brief  Slice is a simple structure that contains a length and a pointer to an
  * external array, which is motivated from leveldb
@@ -15,7 +15,7 @@
 
 namespace dmlc {
 
-/**
+/*!
  * \brief Slice is a simple structure containing a pointer into some external
  * storage and a size. The user of a Slice must ensure that the slice is not
  * used after the corresponding external storage has been deallocated.
@@ -25,25 +25,25 @@ namespace dmlc {
 template <typename T>
 class Slice {
  public:
-  /** \brief Create an empty slice. */
+  /*! \brief Create an empty slice. */
   Slice() : data_(NULL), size_(0) { }
 
-  /** \brief Create a slice that refers to d[0,n-1].
+  /*! \brief Create a slice that refers to d[0,n-1].
   Slice(const T* d, size_t n) : data_(d), size_(n) { }
 
-  /** \brief Create a slice from std::vector */
+  /*! \brief Create a slice from std::vector */
   Slice(const std::vector<T>& v) : data_(v.data()), size_(v.size()) { }
 
-  /** \brief Return a pointer to the beginning of the referenced data */
+  /*! \brief Return a pointer to the beginning of the referenced data */
   const char* data() const { return data_; }
 
-  /** \brief Return the length (in bytes) of the referenced data */
+  /*! \brief Return the length (in bytes) of the referenced data */
   size_t size() const { return size_; }
 
-  /** \brief Return true iff the length of the referenced data is zero */
+  /*! \brief Return true iff the length of the referenced data is zero */
   bool empty() const { return size_ == 0; }
 
-  /** \brief Return the ith byte in the referenced data.
+  /*! \brief Return the ith byte in the referenced data.
    *  REQUIRES: n < size()
    */
   T operator[] (size_t n) const {
@@ -51,7 +51,7 @@ class Slice {
     return data_[n];
   }
 
-  /** \brief Change this slice to refer to an empty array */
+  /*! \brief Change this slice to refer to an empty array */
   void clear() { data_ = NULL; size_ = 0; }
 
   Slice segment(int pos, int n) const {
@@ -63,20 +63,22 @@ class Slice {
 #if DMLC_USE_EIGEN
   typedef Eigen::Map<
     const Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic> > EigenArrayMap;
-  /**
+  /*!
    * \brief Return a size()/k by k Eigen3 Array
    */
   EigenArrayMap ToEigenArray(int k = 1) const {
-    return EigenArrayMap(data(), size());
+    CHECK_EQ(size() % k, 0);
+    return EigenArrayMap(data(), size() / k, k);
   }
 
   typedef Eigen::Map<
     const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> > EigenMatrixMap;
-  /**
+  /*!
    * \brief Return a size()/k by k Eigen3 Matrix
    */
   EigenMatrixMap ToEigenMatrix(int k = 1) const {
-    return EigenMatrixMap(data(), size());
+    CHECK_EQ(size() % k, 0);
+    return EigenMatrixMap(data(), size() / k, k);
   }
 #endif  // DMLC_USE_EIGEN
 
@@ -92,7 +94,7 @@ class Slice {
 // TODO support string? i.e. construct from string, and ToString
 // TODO support == !=
 // TODO slice itself Segment(int pos, int
-
+// TODO a mutable slice?
 
 }  // namespace dmlc
 
