0f286c86c12e932df252745fe41288d02335b719
muli <muli@cs.cmu.edu>
2015-04-07 15:56:40 -0400

    removed ps

--- test/dmlc_test.mk
+++ test/dmlc_test.mk
@@ -1,9 +1,8 @@
-TEST=test/logging_test test/filesys_test test/dataiter_test test/ps_test
+TEST=test/logging_test test/filesys_test test/dataiter_test
 
 test/logging_test: test/logging_test.cc
 test/filesys_test: test/filesys_test.cc src/io/*.h libdmlc.a
 test/dataiter_test: test/dataiter_test.cc  libdmlc.a
-test/ps_test: test/ps_test.cc include/dmlc/ps.h
 
 $(TEST) :
 	$(CXX) $(CFLAGS) -o $@ $(filter %.cpp %.o %.c %.cc %.a,  $^) $(LDFLAGS)
