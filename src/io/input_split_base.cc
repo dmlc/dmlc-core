7e0b18a6e10cb0f68d8aee6a2ecdba39e3cfbf23
tqchen <tianqi.tchen@gmail.com>
2015-04-20 05:39:44 +0000

    change input spliter to ;

--- src/io/input_split_base.cc
+++ src/io/input_split_base.cc
@@ -61,11 +61,11 @@ InputSplitBase::~InputSplitBase(void) {
 
 void InputSplitBase::InitInputFileInfo(const char *uri) {
   // split by :
-  const char *dlm = ":";
+  const char *dlm = ";";
   std::string uri_ = uri;
   char *p = std::strtok(BeginPtr(uri_), dlm);
   std::vector<URI> vec;
-
+  
   while (p != NULL) {
     URI path(p);
     FileInfo info = filesys_->GetPathInfo(path);
