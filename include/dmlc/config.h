0b87d4777714143f5ab86249b50d239cd28ae023
Minjie Wang <wmjlyjemaine@gmail.com>
2015-05-11 00:43:17 -0400

    use non-template config

--- include/dmlc/config.h
+++ include/dmlc/config.h
@@ -8,7 +8,10 @@
 
 #include <iostream>
 #include <cstring>
+#include <iterator>
 #include <map>
+#include <vector>
+#include <sstream>
 
 /*! \brief namespace for dmlc */
 namespace dmlc {
@@ -17,35 +20,39 @@ namespace dmlc {
  * \brief class for config parser
  */
 class Config {
- public:
-  typedef std::map<std::string, std::string>::const_iterator ConfigIterator;
-  typedef std::pair<std::string, std::string> ConfigEntry;
 
  public:
+  typedef std::pair<std::string, std::string> ConfigEntry;
+  class ConfigIterator;
+
   /*!
    * \brief create empty config
+   * \param whether the config supports multi value
    */
-  Config();
+  Config(bool multi_value = false);
   /*!
    * \brief create config and load content from the given stream
    * \param input stream
+   * \param whether the config supports multi value
    */
-  explicit Config(std::istream& is);
+  explicit Config(std::istream& is, bool multi_value = false);
   /*!
    * \brief clear all the values
    */
   void Clear(void);
   /*!
    * \brief load the contents from the stream
-   * \param the stream as input
+   * \param is the stream as input
    */
   void LoadFromStream(std::istream& is);
   /*!
    * \brief set a key-value pair into the config
    * \param key key
    * \param value value
+   * \param is_string whether the value should be wrapped by quotes in proto string
    */
-  void SetParam(const std::string& key, const std::string& value);
+  template<class T>
+  void SetParam(const std::string& key, const T& value, bool is_string = false);
 
   /*!
    * \brief get the config under the key
@@ -72,9 +79,42 @@ class Config {
   ConfigIterator end() const;
 
  private:
-  std::map<std::string, bool> is_string_map_;
-  std::map<std::string, std::string> config_map_;
+  struct ConfigValue {
+    std::string val;
+    bool is_string;
+  };
+  typedef std::map<std::string, std::vector<ConfigValue>> InternalMap;
+  typedef typename InternalMap::const_iterator InternalMapIterator;
+  void Insert(const std::string& key, const std::string& value, bool is_string);
+
+ public:
+
+  class ConfigIterator : public std::iterator< std::input_iterator_tag, ConfigEntry > {
+   public:
+    ConfigIterator(InternalMapIterator ki, size_t val_index);
+    ConfigIterator(const ConfigIterator& other);
+    ConfigIterator& operator ++ ();
+    ConfigIterator operator ++ (int);
+    bool operator == (const ConfigIterator& rhs) const;
+    bool operator != (const ConfigIterator& rhs) const;
+    ConfigEntry operator * ();
+   private:
+    InternalMapIterator key_iter_;
+    size_t value_index_;
+  };
+
+ private:
+  InternalMap config_map_;
+  bool multi_value_;
+
 };
+
+template<class T>
+void Config::SetParam(const std::string& key, const T& value, bool is_string) {
+  std::ostringstream oss;
+  oss << value;
+  Insert(key, oss.str(), is_string);
+}
 
 } // namespace dmlc
 
