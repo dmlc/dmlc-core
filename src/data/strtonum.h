978ec8816144e332f820ddb9822e30c4c5a69604
Chuntao Hong <chuntao.hong@gmail.com>
2015-06-09 15:28:37 +0800

    fix handling of scientific numbers

--- src/data/strtonum.h
+++ src/data/strtonum.h
@@ -23,7 +23,10 @@ inline bool isdigit(char c) {
 }
 
 inline bool isdigits(char c) {
-  return (c >= '0' && c <= '9') || c == '+' || c == '-' || c == '.';
+  return (c >= '0' && c <= '9') 
+    || c == '+' || c == '-' 
+    || c == '.' 
+    || c == 'e' || c == 'E';
 }
 
 /*!
