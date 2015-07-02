f05ba71268573cdb8c5547396abd53a2eb13190f
tqchen <tianqi.tchen@gmail.com>
2015-07-02 12:14:15 -0700

    update

--- scripts/travis/travis_script.sh
+++ scripts/travis/travis_script.sh
@@ -4,14 +4,12 @@ export MAKECMD= make -j4
 
 if [ ${TASK} == "lint" ]; then
     ${MAKECMD} lint
-    exit 0
 fi
 
 if [ ${TASK} == "build" ]; then
     cp make/config.mk .
     echo "USE_S3=1" >> config.mk
     ${MAKECMD} all
-    exit 0
 fi
 
 if [ ${TASK} == "build_test" ]; then
@@ -19,5 +17,4 @@ if [ ${TASK} == "build_test" ]; then
     echo "USE_S3=1" >> config.mk
     echo "BUILD_TEST=1" >> config.mk
     ${MAKECMD} all
-    exit 0
 fi
