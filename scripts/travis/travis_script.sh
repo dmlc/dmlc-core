0db25c146763a9690b8c0f293b0d8c9cee632b07
tqchen <tianqi.tchen@gmail.com>
2015-07-02 12:44:22 -0700

    add clang

--- scripts/travis/travis_script.sh
+++ scripts/travis/travis_script.sh
@@ -2,18 +2,20 @@
 # Main script of travis
 
 if [ ${TASK} == "lint" ]; then
-    make lint || exit -1
+    make lint
 fi
 
 if [ ${TASK} == "build" ]; then
     cp make/config.mk .
     echo "USE_S3=1" >> config.mk
-    make all || exit -1
+    echo "export CXX="${CXX} >> config.mk
+    make all
 fi
 
 if [ ${TASK} == "build_test" ]; then
     cp make/config.mk .
     echo "USE_S3=1" >> config.mk
     echo "BUILD_TEST=1" >> config.mk
-    make all || exit -1
+    echo "export CXX="${CXX} >> config.mk
+    make all
 fi
