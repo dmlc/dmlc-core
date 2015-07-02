76e2840c2d583064f9b4499d7d5ef5ed11c70513
tqchen <tianqi.tchen@gmail.com>
2015-07-02 12:16:46 -0700

    add bdage

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 DMLC Core
 ====
 
+[![Build Status](https://travis-ci.org/dmlc/dmlc-core.png)](https://travis-ci.org/dmlc/dmlc-core)
+
 A common code-base for Distributed Machine Learning in C++.
 
 Developer Channel [![Join the chat at https://gitter.im/dmlc/dmlc-core](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/dmlc/dmlc-core?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
--- scripts/travis/travis_script.sh
+++ scripts/travis/travis_script.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # Main script of travis 
-export MAKECMD= make -j4
 
 if [ ${TASK} == "lint" ]; then
-    ${MAKECMD} lint
+    make lint
 fi
 
 if [ ${TASK} == "build" ]; then
     cp make/config.mk .
     echo "USE_S3=1" >> config.mk
-    ${MAKECMD} all
+    make all
 fi
 
 if [ ${TASK} == "build_test" ]; then
     cp make/config.mk .
     echo "USE_S3=1" >> config.mk
     echo "BUILD_TEST=1" >> config.mk
-    ${MAKECMD} all
+    make all
 fi
