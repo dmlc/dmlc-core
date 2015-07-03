cb95d1ee1d4924f71263680bd7fae1c4567dcf02
tqchen <tianqi.tchen@gmail.com>
2015-07-03 12:12:16 -0700

    try

--- scripts/packages.mk
+++ scripts/packages.mk
@@ -3,22 +3,15 @@
 # List of packages here
 .PHONY: gtest
 
-ifndef CACHE_PREFIX
-	CACHE_PREFIX = ${HOME}/.cache/usr
-endif
-
 # rules for gtest
 ${CACHE_PREFIX}/include/gtest:
-    rm -rf gtest-1.7.0.zip gtest-1.7.0
-    wget http://googletest.googlecode.com/files/gtest-1.7.0.zip
-    unzip gtest-1.7.0.zip
-    cd gtest-1.7.0
-    ./configure
-    make
-    cp -r include/gtest ${CACHE_PREFIX}/include
-    cp -r lib/.libs/* ${CACHE_PREFIX}/lib
-    cp -f lib/*.la ${CACHE_PREFIX}/lib
-    cd ..
-    rm -rf gtest-1.7.0.zip
+	rm -rf gtest-1.7.0.zip gtest-1.7.0
+	wget http://googletest.googlecode.com/files/gtest-1.7.0.zip
+	unzip gtest-1.7.0.zip
+	cd gtest-1.7.0 ; ./configure; make; cd -
+	cp -r gtest-1.7.0/include/gtest ${CACHE_PREFIX}/include
+	cp -r gtest-1.7.0/lib/.libs/* ${CACHE_PREFIX}/lib
+	cp -f gtest-1.7.0/lib/*.la ${CACHE_PREFIX}/lib
+	rm -rf gtest-1.7.0.zip
 
 gtest: | ${CACHE_PREFIX}/include/gtest
