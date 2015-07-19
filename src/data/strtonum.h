aaaeb74a6ac51934bc967f2bdbea16ecdf23f965
muli <muli@cs.cmu.edu>
2015-07-19 05:24:06 +0000

    add nolint

--- src/data/strtonum.h
+++ src/data/strtonum.h
@@ -179,10 +179,11 @@ class Str2T<uint64_t> {
 };
 
 template<>
-class Str2T<unsigned long long> {
+class Str2T<unsigned long long> {  // NOLINT(*)
  public:
-  static inline unsigned long long get(const char * begin, const char * end) {
-    return strtoint<unsigned long long>(begin, NULL, 10);
+  static inline unsigned long long get(  // NOLINT(*)
+      const char * begin, const char * end) {
+    return strtoint<unsigned long long>(begin, NULL, 10);  // NOLINT(*)
   }
 };
 
