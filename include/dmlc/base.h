2849d40137d6502df8ca5ab33dd307cf5923db87
Yutian Li <hotpxless@gmail.com>
2015-07-28 02:01:50 +0800

    [concurrency-fix-style] disable copy and assign

--- include/dmlc/base.h
+++ include/dmlc/base.h
@@ -53,6 +53,28 @@
 #endif
 #endif
 
+/*!
+ * \brief Disable copy constructor and assignment operator.
+ *
+ * If C++11 is supported, both copy and move constructors and
+ * assignment operators are deleted explicitly. Otherwise, they are
+ * only declared but not implemented. Place this macro in private
+ * section if C++11 is not available.
+ */
+#ifndef DISALLOW_COPY_AND_ASSIGN
+#  if DMLC_USE_CXX11
+#    define DISALLOW_COPY_AND_ASSIGN(T) \
+       T(T const&) = delete; \
+       T(T&&) = delete; \
+       T& operator=(T const&) = delete; \
+       T& operator=(T&&) = delete;
+#  else
+#    define DISALLOW_COPY_AND_ASSIGN(T) \
+       T(T const&); \
+       T(T&&);
+#  endif
+#endif
+
 ///
 /// code block to handle optionally loading
 ///
