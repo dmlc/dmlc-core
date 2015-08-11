cd19bdf0d411869ab549214dc35801c948a9ee49
sneakerkg <xiaotj1990327@gmail.com>
2015-08-12 00:53:01 +0800

    merge functions in RandomSampler

--- include/dmlc/random.h
+++ include/dmlc/random.h
@@ -13,13 +13,13 @@
 
 /*! \brief namespace for dmlc */
 namespace dmlc {
-/*! \brief random shuffle based on c++11 */
-class Shuffler {
+/*! \brief random sampler based on c++11 */
+class RandomSampler {
  public:
   /*!
    * \brief constructor without seed
    */ 
-  Shuffler(void) {
+  RandomSampler(void) {
     std::random_device rd;
     this->rengine_ = std::mt19937(rd());
   }
@@ -27,7 +27,7 @@ class Shuffler {
    * \brief constructor with seed
    * \param seed the random number seed
    */ 
-  explicit Shuffler(unsigned seed) {
+  explicit RandomSampler(unsigned seed) {
     this->rengine_ = std::mt19937(seed);
   }
   /*!
@@ -38,127 +38,58 @@ class Shuffler {
   inline void Shuffle(std::vector<T> *data) {
     std::shuffle(data->begin(), data->end(), rengine_);
   }
+  /*!
+   * \brief get an uint32 less than n based on uniform distribution
+   * \param n maximum number
+   * \return random number required
+   */
+  inline uint32_t NextUInt32(uint32_t n) {
+    return GetUniformInt<uint32_t>(0, n);
+  }
+  /*!
+   * \brief get an integer from a to b based on uniform distribution
+   * \param a minimum value
+   * \param b maximum value
+   * \return random number required
+   */
+  template< class IntType = int >
+  inline IntType GetUniformInt(IntType a, IntType b) {
+    std::uniform_int_distribution<IntType> dis(a, b);
+    return dis(rengine_);
+  }
+  /*!
+   * \brief get a double less between 0 and 1 based on uniform distribution
+   * \return random number required
+   */
+  inline double NextDouble() {
+    return GetUniformReal<>(0.0, 1.0);
+  }
+  /*!
+   * \brief get an real number from a to b based on uniform distribution
+   * \param a minimum value
+   * \param b maximum value
+   * \return random number required
+   */
+  template< class RealType = double >
+  inline RealType GetUniformReal(RealType a, RealType b) {
+    std::uniform_real_distribution<RealType> dis(a, b);
+    return dis(rengine_);
+  }
+  /*!
+   * \brief get an real number based on gaussian distribution
+   * \param mean mean value
+   * \param stddev standard deviation
+   * \return random number required
+   */
+  template< class RealType = double >
+  inline RealType GetGaussian(RealType mean, RealType stddev) {
+    std::normal_distribution<RealType> dis(mean, stddev);
+    return dis(rengine_);
+  }
 
  private:
   unsigned rseed_;
   std::mt19937 rengine_;
-};
-/*! 
- * \brief int uniform sampler based on c++11
- *        P(i/a,b) = 1 / (b - a + 1) 
- */
-template< class IntType = int >
-class UniformIntSampler {
- public:
-  /*!
-   * \brief constructor without seed
-   * \param a minimum value
-   * \param b maximum value
-   */
-  UniformIntSampler(IntType a, IntType b) :
-      dis(std::uniform_int_distribution<IntType>(a, b)) {
-    std::random_device rd;
-    this->rengine_ = std::mt19937(rd());
-  }
-  /*!
-   * \brief constructor with seed
-   * \param a minimum value
-   * \param b maximum value
-   * \param seed random seed
-   */ 
-  UniformIntSampler(IntType a, IntType b, unsigned seed) :
-      dis(std::uniform_int_distribution<IntType>(a, b)) {
-    this->rengine_ = std::mt19937(seed);
-  }
-  /*!
-   * \brief Get the next value in the distribution
-   * \return IntType can be int, long
-   */ 
-  inline IntType Get() {
-    return dis(rengine_);
-  }
-
- private:
-  std::mt19937 rengine_;
-  std::uniform_int_distribution<> dis;
-};
-/*! 
- * \brief float uniform sampler based on c++11
- *        P(i/a,b) = 1 / (b - a) 
- */
-template< class RealType = double >
-class UniformRealSampler {
- public:
-  /*!
-   * \brief constructor without seed
-   * \param a minimum value
-   * \param b maximum value
-   */
-  UniformRealSampler(RealType a, RealType b) :
-      dis(std::uniform_real_distribution<RealType>(a, b)) {
-    std::random_device rd;
-    this->rengine_ = std::mt19937(rd());
-  }
-  /*!
-   * \brief constructor with seed
-   * \param a minimum value
-   * \param b maximum value
-   * \param seed random seed
-   */ 
-  UniformRealSampler(RealType a, RealType b, unsigned seed) :
-      dis(std::uniform_real_distribution<RealType>(a, b)) {
-    this->rengine_ = std::mt19937(seed);
-  }
-  /*!
-   * \brief Get the next value in the distribution
-   * \return RealType can be float, double
-   */ 
-  inline RealType Get() {
-    return dis(rengine_);
-  }
-
- private:
-  std::mt19937 rengine_;
-  std::uniform_real_distribution<> dis;
-};
-/*! 
- * \brief float gaussian sampler based on c++11
- *        P(x/mean,stddev)
- */
-template< class RealType = double >
-class GaussianSampler {
- public:
-  /*!
-   * \brief constructor without seed
-   * \param mean value
-   * \param stddev standard deviation 
-   */
-  GaussianSampler(RealType mean, RealType stddev) :
-      dis(std::normal_distribution<RealType>(mean, stddev)) {
-    std::random_device rd;
-    this->rengine_ = std::mt19937(rd());
-  }
-  /*!
-   * \brief constructor with seed
-   * \param mean value
-   * \param stddev standard deviation
-   * \param seed random seed
-   */ 
-  GaussianSampler(RealType mean, RealType stddev, unsigned seed) :
-      dis(std::normal_distribution<RealType>(mean, stddev)) {
-    this->rengine_ = std::mt19937(seed);
-  }
-  /*!
-   * \brief Get the next value in the distribution
-   * \return RealType can be float, double
-   */ 
-  inline RealType Get() {
-    return dis(rengine_);
-  }
-
- private:
-  std::mt19937 rengine_;
-  std::normal_distribution<> dis;
 };
 }  // namespace dmlc
 #endif  // DMLC_RANDOM_H_
--- test/random_test.cc
+++ test/random_test.cc
@@ -4,6 +4,9 @@
 #include <iterator>
 #include <dmlc/random.h>
 #include <dmlc/recordio.h>
+#include <time.h>
+
+using namespace std;
 
 int main(int argc, char *argv[]) {  
   if (argc < 2) {
@@ -12,29 +15,36 @@ int main(int argc, char *argv[]) {
   }
   unsigned seed = atoi(argv[1]);
   using namespace dmlc;
-  // shuffle
+  RandomSampler rnd(seed);
+  // Shuffle Test
   std::cout << "Shuffle from 1 to 10\n";
   std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  Shuffler shuf(seed);
-  shuf.Shuffle(&v);
+  rnd.Shuffle(&v);
   std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
   std::cout << "\n";
-  
+  // Uniform Int Test
   std::cout << "Uniform Int, Range: 0 - 100 \n";
-  UniformIntSampler<int> intsampler(0, 100, seed);
   for (int n=0; n<10; ++n)
-      std::cout << intsampler.Get() << ' ';
+      std::cout << rnd.GetUniformInt(0, 100) << ' ';
   std::cout << '\n';
-
-  std::cout << "Uniform Real, Range: 0 - 100 \n";
-  UniformRealSampler<double> doublesampler(0.0, 100.0, seed);
+  // Uniform Real Test
+  std::cout << "Uniform Real, Range: 0 - 1 \n";
   for (int n=0; n<10; ++n)
-      std::cout << doublesampler.Get() << ' ';
+      std::cout << rnd.GetUniformReal(0.0, 1.0) << ' ';
   std::cout << '\n';
-
+  // Gaussian Test
   std::cout << "Gaussian Distribution, Mean: 0, Standard Deviation: 10 \n";
-  GaussianSampler<double> gaussiansampler(0.0, 10.0, seed);
   for (int n=0; n<10; ++n)
-      std::cout << gaussiansampler.Get() << ' ';
+      std::cout << rnd.GetGaussian(0.0, 10.0) << ' ';
   std::cout << '\n';
+
+  // Speed Test : Generate 20,000,000 gaussian
+  std::cout << "Speed Test \n";
+  time_t start, end;
+  double res = 0;
+  time(&start);
+  for (int i = 0; i < 20000000; i++)
+    res += rnd.GetGaussian(0.0, 1.0);
+  time(&end);
+  std::cout << "Get Result: " << res << " in " << difftime(end, start) << "s\n";
 }
