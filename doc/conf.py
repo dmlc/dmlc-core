b54f9946acaae5f67ddf1f3aa2e89fd2aea1200d
tqchen <tianqi.tchen@gmail.com>
2015-08-23 12:16:15 -0700

    fix sp

--- doc/conf.py
+++ doc/conf.py
@@ -14,9 +14,12 @@
 import sys
 import os, subprocess
 import shlex
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+sys.path.insert(0, curr_path)
 from sphinx_util import MarkdownParser, AutoStructify
 
 # -- General configuration ------------------------------------------------
