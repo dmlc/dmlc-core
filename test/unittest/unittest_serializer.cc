7d3c78428819dc84c4da8ae1f302ba6c6a235a5d
tqchen <tianqi.tchen@gmail.com>
2015-08-30 15:04:47 -0700

    add memory io

--- test/unittest/unittest_serializer.cc
+++ test/unittest/unittest_serializer.cc
@@ -1,4 +1,5 @@
 #include <dmlc/io.h>
+#include <dmlc/memory_io.h>
 #include <dmlc/logging.h>
 #include <gtest/gtest.h>
 #include <sstream>
@@ -8,51 +9,10 @@
 
 using namespace std;
 
-/*! \brief a in memory buffer that can be read and write as stream interface */
-struct MemoryBufferStream : public dmlc::SeekStream {
- public:
-  explicit MemoryBufferStream(std::string *p_buffer)
-      : p_buffer_(p_buffer) {
-    curr_ptr_ = 0;
-  }
-  virtual ~MemoryBufferStream(void) {}
-  virtual size_t Read(void *ptr, size_t size) {
-    CHECK(curr_ptr_ <= p_buffer_->length())
-        << "read can not have position excceed buffer length";
-    size_t nread = std::min(p_buffer_->length() - curr_ptr_, size);
-    if (nread != 0) std::memcpy(ptr, &(*p_buffer_)[0] + curr_ptr_, nread);
-    curr_ptr_ += nread;
-    return nread;
-  }
-  virtual void Write(const void *ptr, size_t size) {
-    if (size == 0) return;
-    if (curr_ptr_ + size > p_buffer_->length()) {
-      p_buffer_->resize(curr_ptr_+size);
-    }
-    std::memcpy(&(*p_buffer_)[0] + curr_ptr_, ptr, size);
-    curr_ptr_ += size;
-  }
-  virtual void Seek(size_t pos) {
-    curr_ptr_ = static_cast<size_t>(pos);
-  }
-  virtual size_t Tell(void) {
-    return curr_ptr_;
-  }
-  virtual bool AtEnd(void) const {
-    return curr_ptr_ == p_buffer_->length();
-  }
-
- private:
-  /*! \brief in memory buffer */
-  std::string *p_buffer_;
-  /*! \brief current pointer */
-  size_t curr_ptr_;
-};  // class MemoryBufferStream
-
 template<typename T>
 inline void TestSaveLoad(T data) {
   std::string blob;
-  MemoryBufferStream fs(&blob);
+  dmlc::MemoryStringStream fs(&blob);
   {
     T temp(data);
     static_cast<dmlc::Stream*>(&fs)->Write(temp);
