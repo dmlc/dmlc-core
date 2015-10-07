7e224a9bbc4c652771eaf964a8cb05b315281c4d
yajiedesign <yajiedesign@gmail.com>
2015-10-07 22:22:54 +0800

    fix python lint

--- scripts/lint3.py
+++ scripts/lint3.py
@@ -163,7 +163,7 @@ def main():
             for root, dirs, files in os.walk(path):
                 for name in files:
                     process(os.path.join(root, name), allow_type)
-    
+
     nerr = _HELPER.print_summary(sys.stderr)
     sys.exit(nerr > 0)
 
