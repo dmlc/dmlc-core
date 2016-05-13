9fd3b48462a7a651e12a197679f71e043dcb25a2
Bing Xu <antinucleon@gmail.com>
2016-05-13 02:40:58 -0700

    update travis env

--- scripts/travis/travis_setup_env.sh
+++ scripts/travis/travis_setup_env.sh
@@ -32,9 +32,9 @@ fi
 
 # setup CUDA path if NVCC_PREFIX exists
 if [ ! -z "$NVCC_PREFIX" ]; then
-    export PATH=${PATH}:${NVCC_PREFIX}/usr/local/cuda-7.0/bin
-    export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${NVCC_PREFIX}/usr/local/cuda-7.0/include
-    export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${NVCC_PREFIX}/usr/local/cuda-7.0/include
-    export LIBRARY_PATH=${LIBRARY_PATH}:${NVCC_PREFIX}/usr/local/cuda-7.0/lib64:${NVCC_PREFIX}/usr/lib/x86_64-linux-gnu
-    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${NVCC_PREFIX}/usr/local/cuda-7.0/lib64:${NVCC_PREFIX}/usr/lib/x86_64-linux-gnu
+    export PATH=${PATH}:${NVCC_PREFIX}/usr/local/cuda-7.5/bin
+    export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${NVCC_PREFIX}/usr/local/cuda-7.5/include
+    export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${NVCC_PREFIX}/usr/local/cuda-7.5/include
+    export LIBRARY_PATH=${LIBRARY_PATH}:${NVCC_PREFIX}/usr/local/cuda-7.5/lib64:${NVCC_PREFIX}/usr/lib/x86_64-linux-gnu
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${NVCC_PREFIX}/usr/local/cuda-7.5/lib64:${NVCC_PREFIX}/usr/lib/x86_64-linux-gnu
 fi
