a880906133b0ea4e515dbc0d497bbd4b5519efda
AbdealiJK <abdealikothari@gmail.com>
2016-11-01 09:01:12 +0530

    uri_spec.h: Show better message for URI parse error

--- src/io/uri_spec.h
+++ src/io/uri_spec.h
@@ -62,8 +62,10 @@ class URISpec {
       for (size_t i = 0; i < arg_list.size(); ++i) {
         std::istringstream is(arg_list[i]);
         std::pair<std::string, std::string> kv;
-        CHECK(std::getline(is, kv.first, '=')) << "Invalid uri argument format";
-        CHECK(std::getline(is, kv.second)) << "Invalid uri argument format";
+        CHECK(std::getline(is, kv.first, '=')) << "Invalid uri argument format"
+          << " for key in arg " << i + 1;
+        CHECK(std::getline(is, kv.second)) << "Invalid uri argument format"
+          << " for value in arg " << i + 1;
         this->args.insert(kv);
       }
     } else {
