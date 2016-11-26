78b78be34ac27d30f2193f3d51848c62887669c4
Simon DENEL <simondenel1@gmail.com>
2016-11-26 21:10:34 +0100

    Adding omp_get_max_threads (#191)

--- include/dmlc/omp.h
+++ include/dmlc/omp.h
@@ -17,6 +17,7 @@
 //! \cond Doxygen_Suppress
 inline int omp_get_thread_num() { return 0; }
 inline int omp_get_num_threads() { return 1; }
+inline int omp_get_max_threads() { return 1; }
 inline int omp_get_num_procs() { return 1; }
 inline void omp_set_num_threads(int nthread) {}
 #endif
