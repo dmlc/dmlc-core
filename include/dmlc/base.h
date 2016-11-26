f8f1df3ee8ac0d8c604ad4e625ede1cdb859c830
Junyuan Xie <eric.jy.xie@gmail.com>
2016-11-26 00:17:27 -0800

    print value and stack trace

--- include/dmlc/base.h
+++ include/dmlc/base.h
@@ -37,6 +37,14 @@
 #define DMLC_LOG_CUSTOMIZE 0
 #endif
 
+/*!
+ * \brief Wheter to print stack trace for fatal error,
+ * enabled on linux when using gcc.
+ */
+#if (!defined(DMLC_LOG_STACK_TRACE) && defined(__GNUC__))
+#define DMLC_LOG_STACK_TRACE 1
+#endif
+
 /*! \brief whether compile with hdfs support */
 #ifndef DMLC_USE_HDFS
 #define DMLC_USE_HDFS 0
