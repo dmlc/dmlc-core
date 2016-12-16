dc3da8f5cda80ed0b882cd5d67cd52a8f85f8a13
AbdealiJK <abdealikothari@gmail.com>
2016-12-16 09:21:06 +0530

    travis

--- test/unittest/unittest_csv_parser.cc
+++ test/unittest/unittest_csv_parser.cc
@@ -21,21 +21,21 @@ TEST(CSVParser, basics) {
 
   parser->BeforeFirst();
   ASSERT_TRUE(parser->Next());
-  dmlc::RowBlock<unsigned> block = parser->Value();
-  ASSERT_EQ(block.size, 2);
-  EXPECT_FLOAT_EQ(block[0].label, 0);
-  EXPECT_FLOAT_EQ(block[1].label, 0);
-  EXPECT_FLOAT_EQ(block[0].weight, 1);
-  EXPECT_FLOAT_EQ(block[1].weight, 1);
-
-  std::vector<dmlc::real_t> row1 = {0, 0.1, 100, 5};
-  std::vector<dmlc::real_t> row2 = {1, 0.2, 100, 10};
-  EXPECT_EQ(block[0].length, block[1].length);
-  for (unsigned j = 0; j < block[0].length; ++j) {
-    EXPECT_EQ(block[0].get_index(j), j);
-    EXPECT_EQ(block[1].get_index(j), j);
-    EXPECT_FLOAT_EQ(block[0].value[j], row1[j]);
-    EXPECT_FLOAT_EQ(block[1].value[j], row2[j]);
+  std::vector<std::vector<dmlc::real_t> > expected = { {0, 0.1, 100, 5},
+                                                       {1, 0.2, 100, 10} };
+  unsigned cur_row = 0;
+  while (parser->Next()) {
+    dmlc::RowBlock<unsigned> block = parser->Value();
+    for (unsigned iblock = 0; iblock < block.size; ++iblock) {
+      EXPECT_FLOAT_EQ(block[iblock].label, 0);
+      EXPECT_FLOAT_EQ(block[iblock].weight, 1);
+      EXPECT_EQ(block[iblock].length, expected[cur_row].size());
+      for (unsigned icol = 0; icol < block[iblock].length; ++icol) {
+        EXPECT_EQ(block[iblock].get_index(icol), icol);
+        EXPECT_FLOAT_EQ(block[iblock].value[icol], expected[cur_row][icol]);
+      }
+      cur_row += 1;
+    }
   }
 
   std::remove(tmp_file.c_str());
