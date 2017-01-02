c721e25de922c4c09869fdab45fa8679eaa41133
Vadim Khotilovich <khotilovich@gmail.com>
2017-01-01 19:20:46 -0600

    disable stack-tracing in MinGW

--- include/dmlc/base.h
+++ include/dmlc/base.h
@@ -41,7 +41,7 @@
  * \brief Wheter to print stack trace for fatal error,
  * enabled on linux when using gcc.
  */
-#if (!defined(DMLC_LOG_STACK_TRACE) && defined(__GNUC__))
+#if (!defined(DMLC_LOG_STACK_TRACE) && defined(__GNUC__) && !defined(__MINGW32__))
 #define DMLC_LOG_STACK_TRACE 1
 #endif
 
