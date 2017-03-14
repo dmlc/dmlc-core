c2871f5db50830f5278ff6e323e8e51a6d5516dd
Tianqi Chen <tqchen@users.noreply.github.com>
2017-03-13 20:27:52 -0700

    Update travis_setup_env.sh

--- scripts/travis/travis_setup_env.sh
+++ scripts/travis/travis_setup_env.sh
@@ -1,9 +1,9 @@
 # script to be sourced in travis yml
 # setup all enviroment variables
 
+export CACHE_PREFIX=${HOME}/.cache/usr
 export PATH=${HOME}/.local/bin:${PATH}
 export PATH=${PATH}:${CACHE_PREFIX}/bin
-export CACHE_PREFIX=${HOME}/.cache/usr
 export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${CACHE_PREFIX}/include
 export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${CACHE_PREFIX}/include
 export LIBRARY_PATH=${LIBRARY_PATH}:${CACHE_PREFIX}/lib
