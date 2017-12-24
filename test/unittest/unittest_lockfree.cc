c07f382c330e3185a6794839b0d0f126030e8779
cjolivier01 <cjolivier01@gmail.com>
2017-12-24 11:19:23 -0800

    Refreshed branch sync

--- test/unittest/unittest_lockfree.cc
+++ test/unittest/unittest_lockfree.cc
@@ -3,135 +3,61 @@
 #include <dmlc/memory_io.h>
 #include <dmlc/concurrentqueue.h>
 #include <dmlc/blockingconcurrentqueue.h>
+#include <dmlc/thread_group.h>
 #include <gtest/gtest.h>
-#include <mutex>
-#include <condition_variable>
-
-/*!
- * \brief Simple manually-signalled event gate which remains open
- */
-class SimpleEvent {
- public:
-  SimpleEvent()
-    : signaled_(false) {}
-  void wait() {
-    std::unique_lock<std::mutex> lock(mutex_);
-    if (!signaled_) {
-      condition_variable_.wait(lock);
-    }
-  }
-  void signal() {
-    signaled_ = true;
-    std::unique_lock<std::mutex> lk(mutex_);
-    condition_variable_.notify_all();
-  }
-
-  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
-  struct SetReadyOnDestroy {
-    explicit inline SetReadyOnDestroy(std::shared_ptr<SimpleEvent> *event)
-      : event_(*event) {
-    }
-    inline ~SetReadyOnDestroy() {
-      if (event_) {
-        event_->signal();
-      }
-    }
-    std::shared_ptr<SimpleEvent>  event_;
-  };
-
- private:
-  std::mutex              mutex_;
-  std::condition_variable condition_variable_;
-  std::atomic<bool>       signaled_;
-};
-
-/*!
- * \brief Simple thread lifecycle management
- */
-class ThreadGroup {
- public:
-  ~ThreadGroup() {
-    WaitForAll();
-  }
-  size_t Count() const {
-    std::unique_lock<std::mutex> lk(cs_threads_);
-    return threads_.size();
-  }
-  void WaitForAll() {
-    while(Count()) {
-      std::shared_ptr<std::thread> thrd(nullptr);
-      do {
-        std::unique_lock<std::mutex> lk(cs_threads_);
-        if (!threads_.empty()) {
-          thrd = *threads_.begin();
-          threads_.erase(thrd);
-        }
-      } while (false);
-      if(thrd) {
-        if(thrd->joinable()) {
-          thrd->join();
-        }
-      }
-    }
-  }
-  template<typename StartFunction, typename ...Args>
-  void Start(StartFunction start_function, Args ...args) {
-    std::unique_lock<std::mutex> lk(cs_threads_);
-    std::shared_ptr<std::thread> thrd = std::make_shared<std::thread>(start_function, args...);
-    threads_.insert(thrd);
-  }
- private:
-  mutable std::mutex                               cs_threads_;
-  std::unordered_set<std::shared_ptr<std::thread>> threads_;
-};
 
 template<typename TQueue>
 struct LFQThreadData {
   LFQThreadData() : count_(0) {}
   std::atomic<size_t> count_;
   std::shared_ptr<TQueue> q_ = std::make_shared<TQueue>();
-  std::shared_ptr<SimpleEvent> ready_ = std::make_shared<SimpleEvent>();
+  std::shared_ptr<dmlc::ManualEvent> ready_ = std::make_shared<dmlc::ManualEvent>();
   std::mutex cs_map_;
   std::set<int> thread_map_;
 };
 
 template<typename TQueue>
-static void PushThread(const int id, std::shared_ptr<LFQThreadData<TQueue>> data) {
+static int PushThread(const int id, std::shared_ptr<LFQThreadData<TQueue>> data) {
   ++data->count_;
   data->ready_->wait();
   data->q_->enqueue(id);
   std::unique_lock<std::mutex> lk(data->cs_map_);
   data->thread_map_.erase(id);
+  return 0;
 }
 
 template<typename TQueue>
-static void PullThread(const int id, std::shared_ptr<LFQThreadData<TQueue>> data) {
+static int PullThread(const int id, std::shared_ptr<LFQThreadData<TQueue>> data) {
   ++data->count_;
   data->ready_->wait();
   int val;
-  GTEST_ASSERT_EQ(data->q_->try_dequeue(val), true);
+  CHECK_EQ(data->q_->try_dequeue(val), true);
   std::unique_lock<std::mutex> lk(data->cs_map_);
   data->thread_map_.erase(id);
+  return 0;
 }
 
 template<typename TQueue>
-static void BlockingPullThread(const int id, std::shared_ptr<LFQThreadData<TQueue>> data) {
+static int BlockingPullThread(const int id, std::shared_ptr<LFQThreadData<TQueue>> data) {
   ++data->count_;
   data->ready_->wait();
   int val;
   data->q_->wait_dequeue(val);
   std::unique_lock<std::mutex> lk(data->cs_map_);
   data->thread_map_.erase(id);
+  return 0;
 }
 
+static inline std::string TName(const std::string& s, int x) { return s + "-" + std::to_string(x); }
+
 TEST(Lockfree, ConcurrentQueue) {
-  ThreadGroup threads;
-  const size_t ITEM_COUNT = 100;
+  dmlc::ThreadGroup threads;
+  const int ITEM_COUNT = 100;
   auto data = std::make_shared<LFQThreadData<dmlc::moodycamel::ConcurrentQueue<int>>>();
   for(size_t x = 0; x < ITEM_COUNT; ++x) {
     std::unique_lock<std::mutex> lk(data->cs_map_);
     data->thread_map_.insert(x);
-    threads.Start(PushThread<dmlc::moodycamel::ConcurrentQueue<int>>, x, data);
+    threads.create(TName("PushThread", x), true, PushThread<dmlc::moodycamel::ConcurrentQueue<int>>, x, data);
   }
   while(data->count_ < ITEM_COUNT) {
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
@@ -147,34 +73,38 @@ TEST(Lockfree, ConcurrentQueue) {
   size_t count = data->q_->size_approx();
   GTEST_ASSERT_EQ(count, ITEM_COUNT);
 
-  threads.WaitForAll();
-  GTEST_ASSERT_EQ(threads.Count(), 0UL);
+  threads.join_all();
+  GTEST_ASSERT_EQ(threads.size(), 0);
 
   for(size_t x = 0; x < ITEM_COUNT; ++x) {
     std::unique_lock<std::mutex> lk(data->cs_map_);
     data->thread_map_.insert(x);
-    threads.Start(PullThread<dmlc::moodycamel::ConcurrentQueue<int>>, x, data);
+    // Just to mix things up, don't auto-remove
+    threads.create(TName("PullThread", x), false, PullThread<dmlc::moodycamel::ConcurrentQueue<int>>, x, data);
   }
   data->ready_->signal();
-  threads.WaitForAll();
-  GTEST_ASSERT_EQ(threads.Count(), 0UL);
+  threads.join_all();
+  GTEST_ASSERT_EQ(threads.size(), 0);
 
   count = data->q_->size_approx();
   GTEST_ASSERT_EQ(count, 0UL);
 }
 
 TEST(Lockfree, BlockingConcurrentQueue) {
+  using BlockingQueue = dmlc::moodycamel::BlockingConcurrentQueue<
+    int, dmlc::moodycamel::ConcurrentQueueDefaultTraits>;
 
   using BlockingQueue = dmlc::moodycamel::BlockingConcurrentQueue<
     int, dmlc::moodycamel::ConcurrentQueueDefaultTraits>;
 
-  ThreadGroup threads;
-  const size_t ITEM_COUNT = 100;
+  dmlc::ThreadGroup threads;
+  const int ITEM_COUNT = 100;
   auto data = std::make_shared<LFQThreadData<BlockingQueue>>();
   for(size_t x = 0; x < ITEM_COUNT; ++x) {
     std::unique_lock<std::mutex> lk(data->cs_map_);
     data->thread_map_.insert(x);
-    threads.Start(PushThread<BlockingQueue>, x, data);
+    // Just to mix things up, don't auto-remove
+    threads.create(TName("PushThread", x), false, PushThread<BlockingQueue>, x, data);
   }
   while(data->count_ < ITEM_COUNT) {
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
@@ -190,17 +120,17 @@ TEST(Lockfree, BlockingConcurrentQueue) {
   size_t count = data->q_->size_approx();
   GTEST_ASSERT_EQ(count, ITEM_COUNT);
 
-  threads.WaitForAll();
-  GTEST_ASSERT_EQ(threads.Count(), 0UL);
+  threads.join_all();
+  GTEST_ASSERT_EQ(threads.size(), 0);
 
   for(size_t x = 0; x < ITEM_COUNT; ++x) {
     std::unique_lock<std::mutex> lk(data->cs_map_);
     data->thread_map_.insert(x);
-    threads.Start(BlockingPullThread<BlockingQueue>, x, data);
+    threads.create(TName("BlockingPullThread", x), true, BlockingPullThread<BlockingQueue>, x, data);
   }
   data->ready_->signal();
-  threads.WaitForAll();
-  GTEST_ASSERT_EQ(threads.Count(), 0UL);
+  threads.join_all();
+  GTEST_ASSERT_EQ(threads.size(), 0);
 
   count = data->q_->size_approx();
   GTEST_ASSERT_EQ(count, 0UL);
---
+++ test/unittest/unittest_thread_group.cc
@@ -0,0 +1,193 @@
+#include <dmlc/io.h>
+#include <dmlc/memory_io.h>
+#include <dmlc/blockingconcurrentqueue.h>
+#include <dmlc/thread_group.h>
+#include <gtest/gtest.h>
+#if defined(_MSC_VER) && _MSC_VER <= 1800
+#include <Windows.h>
+#endif
+
+static std::atomic<int> thread_count(0);
+
+static inline std::string TName(const std::string& s, int x) { return s + "-" + std::to_string(x); }
+
+static int this_is_thread_func(std::string label, const bool with_delay) {
+  ++thread_count;
+  if(with_delay) {
+    usleep(1e4);
+  }
+  --thread_count;
+  return 0;
+}
+
+/*!
+ * \brief Generic Thread launch to standalone function, passing ThreadGroup owner
+ */
+TEST(Syc, ThreadLaunchAutoRemove) {
+  std::shared_ptr<dmlc::ThreadGroup> thread_group = std::make_shared<dmlc::ThreadGroup>();
+  for(int x = 0; x < 200; ++x) {
+    dmlc::ThreadGroup::Thread::SharedPtr thread =
+      std::make_shared<dmlc::ThreadGroup::Thread>(std::string("test_thread_ar ")
+                                                         + std::to_string(x), thread_group.get());
+    dmlc::ThreadGroup::Thread::launch(thread, true, this_is_thread_func, "Runner", false);
+  }
+  thread_group.reset();
+  CHECK_EQ(thread_count, 0);
+}
+
+/*!
+ * \brief Generic Thread launch to standalone function, passing ThreadGroup owner
+ */
+TEST(Syc, ThreadLaunchAutoRemoveWithDelay) {
+  std::shared_ptr<dmlc::ThreadGroup> thread_group = std::make_shared<dmlc::ThreadGroup>();
+  for(int x = 0; x < 200; ++x) {
+    dmlc::ThreadGroup::Thread::SharedPtr thread =
+      std::make_shared<dmlc::ThreadGroup::Thread>(std::string("test_thread_rwd ")
+                                                         + std::to_string(x), thread_group.get());
+    dmlc::ThreadGroup::Thread::launch(thread, true, this_is_thread_func, "Runner", true);
+  }
+  thread_group.reset();
+  CHECK_EQ(thread_count, 0);
+}
+
+/*!
+ * \brief Generic Thread launch to standalone function, passing ThreadGroup owner
+ */
+TEST(Syc, ThreadLaunchNoAutoRemove) {
+  std::shared_ptr<dmlc::ThreadGroup> thread_group = std::make_shared<dmlc::ThreadGroup>();
+  for(int x = 0; x < 200; ++x) {
+    dmlc::ThreadGroup::Thread::SharedPtr thread =
+      std::make_shared<dmlc::ThreadGroup::Thread>(std::string("test_thread_nao ")
+                                                         + std::to_string(x), thread_group.get());
+    dmlc::ThreadGroup::Thread::launch(thread, false, this_is_thread_func, "Runner", false);
+  }
+  thread_group.reset();
+  CHECK_EQ(thread_count, 0);
+}
+
+/*!
+ * \brief Generic Thread launch to standalone function, passing ThreadGroup owner
+ */
+TEST(Syc, ThreadLaunchNoAutoRemoveWithDelay) {
+  std::shared_ptr<dmlc::ThreadGroup> thread_group = std::make_shared<dmlc::ThreadGroup>();
+  for(int x = 0; x < 200; ++x) {
+    dmlc::ThreadGroup::Thread::SharedPtr thread =
+      std::make_shared<dmlc::ThreadGroup::Thread>(std::string("test_thread_narwd ")
+                                                         + std::to_string(x), thread_group.get());
+    dmlc::ThreadGroup::Thread::launch(thread, false, this_is_thread_func, "Runner", true);
+  }
+  thread_group.reset();
+  CHECK_EQ(thread_count, 0);
+}
+
+/*!
+ * \brief Test BlockingQueueThread
+ */
+TEST(Syc, ThreadLaunchQueueThread) {
+  // Define the queue type for convenience
+  using BQ = dmlc::BlockingQueueThread<int, -1>;
+
+  // Create the thread group
+  std::shared_ptr<dmlc::ThreadGroup> thread_group = std::make_shared<dmlc::ThreadGroup>();
+
+  // Create the queue thread object
+  std::shared_ptr<BQ> queue_thread = std::make_shared<BQ>("BlockingQueueThread",
+                                                          thread_group.get());
+
+  // Queue some stuff before the thread starts
+  queue_thread->enqueue(1);
+  queue_thread->enqueue(2);
+  queue_thread->enqueue(3);
+  queue_thread->enqueue(4);
+  CHECK_EQ(queue_thread->size_approx(), 4U);
+  // Launch the queue thread, passing queue item handler as lambda
+  BQ::launch_run(queue_thread,
+                 // Queue item handler
+                 [queue_thread](int item) -> int {
+                   std::cout << "ITEM: " << item
+                             << std::endl << std::flush;
+                   if(item >= 2 && item <= 3) {
+                     // Queue some more while thread is running
+                     queue_thread->enqueue(100 + item);
+                   }
+                   return 0;  // return 0 means continue
+                 });
+  // Trigger the queues to exit
+  thread_group->request_shutdown_all();
+  // Wait for all of the queue threads to exit
+  thread_group->join_all();
+  // Check that the queue is empty
+  CHECK_EQ(queue_thread->size_approx(), 0);
+}
+
+using Tick = std::chrono::high_resolution_clock::time_point;
+static inline Tick Now() { return std::chrono::high_resolution_clock::now(); }
+static inline uint64_t GetDurationInNanoseconds(const Tick &t1, const Tick &t2) {
+  return static_cast<uint64_t>(
+    std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count());
+}
+static inline uint64_t GetDurationInNanoseconds(const Tick &since) {
+  return GetDurationInNanoseconds(since, Now());
+}
+
+/*!
+ * \brief Test TimerThread
+ */
+TEST(Syc, TimerThread) {
+  // Create the thread group
+  std::shared_ptr<dmlc::ThreadGroup> thread_group = std::make_shared<dmlc::ThreadGroup>();
+
+  using Duration = std::chrono::milliseconds;
+  // Create the queue thread object
+  std::shared_ptr<dmlc::TimerThread<Duration>> timer_thread =
+    std::make_shared<dmlc::TimerThread<Duration>>("TimerThread", thread_group.get());
+  Tick start_time = Now();
+  size_t count = 0;
+  // Launch the queue thread, passing queue item handler as lambda
+  dmlc::TimerThread<Duration>::start(
+    timer_thread, Duration(10), [timer_thread, start_time, &count]() -> int {
+      std::cout << "TIME: "
+                << (GetDurationInNanoseconds(start_time)/1000/1000)
+                << std::endl << std::flush;
+      ++count;
+      return 0;  // return 0 means continue
+    });
+  std::this_thread::sleep_for(Duration(50));
+  // Trigger the queues to exit
+  thread_group->request_shutdown_all();
+  // Wait for all of the queue threads to exit
+  thread_group->join_all();
+  GTEST_ASSERT_GE(count, 3U);  // Should have at least done three
+  GTEST_ASSERT_LE(count, 10U); // Should not have had time to do 20 of them
+}
+
+/*!
+ * \brief Test TimerThread Simple
+ */
+TEST(Syc, TimerThreadSimple) {
+  // Create the thread group
+  std::shared_ptr<dmlc::ThreadGroup> thread_group = std::make_shared<dmlc::ThreadGroup>();
+
+  using Duration = std::chrono::milliseconds;
+  Tick start_time = Now();
+  size_t count = 0;
+  // Launch the queue thread, passing queue item handler as lambda
+  dmlc::CreateTimer("TimerThreadSimple",
+                    Duration(10),
+                    thread_group.get(),
+                    [start_time, &count]() -> int {
+                      std::cout << "TIME: "
+                                << (GetDurationInNanoseconds(start_time)/1000/1000)
+                                << std::endl << std::flush;
+                      ++count;
+                      return 0;  // return 0 means continue
+                    });
+  std::this_thread::sleep_for(Duration(50));
+  // Trigger the queues to exit
+  thread_group->request_shutdown_all();
+  // Wait for all of the queue threads to exit
+  thread_group->join_all();
+  GTEST_ASSERT_GE(count, 3U);  // Should have at least done three
+  GTEST_ASSERT_LE(count, 10U); // Should not have had time to do 20 of them
+}
+
