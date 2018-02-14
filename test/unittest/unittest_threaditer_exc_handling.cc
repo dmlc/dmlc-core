e91e529f61c4f19b67f45622d01e993ca06b5af4
Anirudh Subramanian <anirudh2290@ufl.edu>
2018-02-14 01:23:44 +0000

    Add test

---
+++ test/unittest/unittest_threaditer_exc_handling.cc
@@ -0,0 +1,74 @@
+#include <chrono>
+#include <dmlc/io.h>
+#include <dmlc/logging.h>
+#include <dmlc/threadediter.h>
+#include <gtest/gtest.h>
+
+using namespace dmlc;
+namespace producer_test {
+inline void delay(int sleep) {
+  if (sleep < 0) {
+    int d = rand() % (-sleep);
+    std::this_thread::sleep_for(std::chrono::milliseconds(d));
+  } else {
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
+  }
+}
+
+// int was only used as example, in real life
+// use big data blob
+struct IntProducerNextExc : public ThreadedIter<int>::Producer {
+  int counter;
+  int maxcap;
+  int sleep;
+  IntProducerNextExc(int maxcap, int sleep)
+      : counter(0), maxcap(maxcap), sleep(sleep) {}
+  virtual void BeforeFirst(void) { counter = 0; }
+  virtual bool Next(int **inout_dptr) {
+    if (counter == maxcap)
+      return false;
+    if (counter == (maxcap - 1)) {
+      counter++;
+      LOG(FATAL) << "Test Throw exception";
+    }
+    // allocate space if not exist
+    if (*inout_dptr == NULL) {
+      *inout_dptr = new int();
+    }
+    delay(sleep);
+    **inout_dptr = counter++;
+    return true;
+  }
+};
+}
+
+TEST(ThreadedIter, exception) {
+  using namespace producer_test;
+  int *value;
+  ThreadedIter<int> iter2;
+  iter2.set_max_capacity(7);
+  IntProducerNextExc prod(5, 100);
+  bool caught = false;
+  iter2.Init(&prod);
+  iter2.BeforeFirst();
+  try {
+    delay(700);
+    iter2.Recycle(&value);
+  } catch (dmlc::Error &e) {
+    caught = true;
+    LOG(INFO) << "recycle exception caught";
+  }
+  CHECK(caught);
+  caught = false;
+  iter2.BeforeFirst();
+  try {
+    while (iter2.Next(&value)) {
+      iter2.Recycle(&value);
+    }
+  } catch (dmlc::Error &e) {
+    caught = true;
+    LOG(INFO) << "next exception caught";
+  }
+  CHECK(caught);
+  LOG(INFO) << "finish";
+}
