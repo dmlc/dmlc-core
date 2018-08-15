ee773cd6ab2a32c07cf3f09ebaf9205ddf0a616e
Anirudh Subramanian <anirudh2290@apache.org>
2018-08-15 14:23:08 -0700

    Add OMPException class and use it for Text Parser (#445)
    * Add OMPException class and use it for Text Parser
    
    * Fix lint
    
    * Add documentation for Rethrow and Run
    
    * Fix lint
    
    * Add documentation for OMPException
    
    * Add more documentation
    
    * Fix comments
    
    * Change capture by value to capture by reference

--- include/dmlc/common.h
+++ include/dmlc/common.h
@@ -9,6 +9,8 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <mutex>
+#include "./logging.h"
 
 namespace dmlc {
 /*!
@@ -43,6 +45,40 @@ template<>
 inline size_t HashCombine<size_t>(size_t key, const size_t& value) {
   return key ^ (value + 0x9e3779b9 + (key << 6) + (key >> 2));
 }
+
+/*!
+ * \brief OMP Exception class catches, saves and rethrows exception from OMP blocks
+ */
+class OMPException {
+ private:
+  // exception_ptr member to store the exception
+  std::exception_ptr omp_exception_;
+  // mutex to be acquired during catch to set the exception_ptr
+  std::mutex mutex_;
+
+ public:
+  /*!
+   * \brief Parallel OMP blocks should be placed within Run to save exception
+   */
+  template <typename Function, typename... Parameters>
+  void Run(Function f, Parameters... params) {
+    try {
+      f(params...);
+    } catch (dmlc::Error &ex) {
+      std::lock_guard<std::mutex> lock(mutex_);
+      if (!omp_exception_) {
+        omp_exception_ = std::current_exception();
+      }
+    }
+  }
+
+  /*!
+   * \brief should be called from the main thread to rethrow the exception
+   */
+  void Rethrow() {
+    if (this->omp_exception_) std::rethrow_exception(this->omp_exception_);
+  }
+};
 
 }  // namespace dmlc
 
