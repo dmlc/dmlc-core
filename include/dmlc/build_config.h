58c16466e06f5c247b5ed0c391f25acde33db412
Philip Cho <chohyu01@cs.washington.edu>
2018-09-05 11:42:07 -0700

    Pass build_config.h to lint check

--- include/dmlc/build_config.h
+++ include/dmlc/build_config.h
@@ -1,8 +1,17 @@
-/* default detection logic for fopen64; may be overriden by CMake */
+/*!
+ * Copyright (c) 2018 by Contributors
+ * \file build_config.h
+ * \brief Default detection logic for fopen64. May be overriden by CMake
+ * \author KOLANICH
+ */
+#ifndef DMLC_BUILD_CONFIG_H_
+#define DMLC_BUILD_CONFIG_H_
 
 #if DMLC_USE_FOPEN64 && \
   (!defined(__GNUC__) || (defined __ANDROID__) || (defined __FreeBSD__) \
-   || (defined __APPLE__) || ((defined __MINGW32__) && !(defined __MINGW64__)))
+  || (defined __APPLE__) || ((defined __MINGW32__) && !(defined __MINGW64__)))
   #warning "Redefining fopen64 with std::fopen"
   #define fopen64 std::fopen
 #endif
+
+#endif  // DMLC_BUILD_CONFIG_H_
