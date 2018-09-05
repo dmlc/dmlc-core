6506a7e670f79224aba3e4e387d9f14160b71e0c
Philip Cho <chohyu01@cs.washington.edu>
2018-09-04 17:33:58 -0700

    Add regression test

--- test/unittest/unittest_inputsplit.cc
+++ test/unittest/unittest_inputsplit.cc
@@ -184,3 +184,45 @@ TEST(InputSplit, test_split_libsvm) {
   ASSERT_EQ(num_row, 5U);
   ASSERT_EQ(num_col, 125U);
 }
+
+TEST(InputSplit, test_split_libsvm_distributed) {
+  {
+    /* Create a test case for partitioned libsvm */
+    TemporaryDirectory tempdir;
+    const char* line
+      = "1 3:1 10:1 11:1 21:1 30:1 34:1 36:1 40:1 41:1 53:1 58:1 65:1 69:1 "
+        "77:1 86:1 88:1 92:1 95:1 102:1 105:1 117:1 124:1\n";
+    const int nfile = 5;
+    for (int file_id = 0; file_id < nfile; ++file_id) {
+      std::string filename
+        = tempdir.path + "/test_" + std::to_string(file_id) + ".libsvm";
+      std::ofstream of(filename.c_str(), std::ios::binary);
+      const int nrepeat = (file_id == 0 ? 6 : 1);
+      for (int i = 0; i < nrepeat; ++i) {
+        of << line;
+      }
+    }
+
+    /* Load the test case with InputSplit and obtain matrix dimensions */
+    const int npart = 2;
+    const size_t expected_dims[npart][2] = { {6, 125}, {4, 125} };
+    for (int part_id = 0; part_id < npart; ++part_id) {
+      std::unique_ptr<dmlc::Parser<uint32_t> > parser(
+        dmlc::Parser<uint32_t>::Create(tempdir.path.c_str(), part_id, npart, "libsvm"));
+      size_t num_row, num_col;
+      CountDimensions(parser.get(), &num_row, &num_col);
+      ASSERT_EQ(num_row, expected_dims[part_id][0]);
+      ASSERT_EQ(num_col, expected_dims[part_id][1]);
+    }
+
+    /* Clean up */
+    for (int file_id = 0; file_id < nfile; ++file_id) {
+      std::string filename
+        = tempdir.path + "/test_" + std::to_string(file_id) + ".libsvm";
+      if (std::remove(filename.c_str()) != 0) {
+        std::cerr << "Couldn't remove file " << filename << std::endl;
+        exit(-1);
+      }
+    }
+  }
+}
