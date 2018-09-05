ac520840d2112ba589e2eccc6b16a48860f1f92a
Philip Cho <chohyu01@cs.washington.edu>
2018-09-04 18:20:28 -0700

    Use existing logic for fopen64 when CMake is not called

--- include/dmlc/build_config.h
+++ include/dmlc/build_config.h
@@ -1,6 +1,8 @@
-//#define FOPEN_64_PRESENT
+/* default detection logic for fopen64; may be overriden by CMake */
 
-#if !defined(FOPEN_64_PRESENT) && DMLC_USE_FOPEN64
+#if DMLC_USE_FOPEN64 && \
+  (!defined(__GNUC__) || (defined __ANDROID__) || (defined __FreeBSD__) \
+   || (defined __APPLE__) || ((defined __MINGW32__) && !(defined __MINGW64__)))
   #warning "Redefining fopen64 with std::fopen"
   #define fopen64 std::fopen
 #endif
