7039180921a74287c4c8e5301d173798242a1231
Philip Cho <chohyu01@cs.washington.edu>
2018-10-01 16:19:57 -0700

    Use GCC 7 for CMake test on Mac

--- scripts/travis/travis_osx_install.sh
+++ scripts/travis/travis_osx_install.sh
@@ -6,6 +6,12 @@ if [ ${TRAVIS_OS_NAME} != "osx" ]; then
     exit 0
 fi
 
+# Update command line tool to avoid an error:
+# "_stdio.h: No such file or directory"
+softwareupdate --list
+softwareupdate --install "Command Line Tools (macOS High Sierra version 10.13) for Xcode-9.4"
+
 brew update
 brew upgrade python3
+brew install gcc@7 || brew link --overwrite gcc@7
 python3 -m pip install --upgrade pip
--- scripts/travis/travis_script.sh
+++ scripts/travis/travis_script.sh
@@ -36,7 +36,11 @@ if [ ${TASK} == "cmake_test" ]; then
     wget -nc https://github.com/google/googletest/archive/release-1.7.0.zip
     unzip -n release-1.7.0.zip
     mv googletest-release-1.7.0 gtest && cd gtest
-    cmake . && make
+    if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+        CC=gcc-7 CXX=g++-7 cmake . && make
+    else
+        cmake . && make
+    fi
     mkdir lib && mv libgtest.a lib
     cd ..
     rm -rf release-1.7.0.zip
@@ -44,7 +48,11 @@ if [ ${TASK} == "cmake_test" ]; then
     # Build dmlc-core with CMake, including unit tests
     rm -rf build
     mkdir build && cd build
-    cmake .. -DGTEST_ROOT=$PWD/../gtest/
+    if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+        CC=gcc-7 CXX=g++-7 cmake .. -DGTEST_ROOT=$PWD/../gtest/
+    else
+        cmake .. -DGTEST_ROOT=$PWD/../gtest/
+    fi
     make
     cd ..
     ./build/test/unittest/dmlc_unit_tests
