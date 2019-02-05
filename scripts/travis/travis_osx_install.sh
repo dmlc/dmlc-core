334c516b890fbe8fa81f802e1d8f71095db81108
Philip Cho <chohyu01@cs.washington.edu>
2019-02-04 19:08:37 -0800

    Improve Travis CI on Mac target

--- scripts/travis/travis_osx_install.sh
+++ scripts/travis/travis_osx_install.sh
@@ -6,12 +6,4 @@ if [ ${TRAVIS_OS_NAME} != "osx" ]; then
     exit 0
 fi
 
-# Update command line tool to avoid an error:
-# "_stdio.h: No such file or directory"
-softwareupdate --list
-softwareupdate --install "Command Line Tools (macOS High Sierra version 10.13) for Xcode-10.1"
-
-brew install gcc@7 || brew link --overwrite gcc@7
-brew update
-brew upgrade python3
 python3 -m pip install --upgrade pip
