26575157860f86c8abd854fa2f1b10cb9934cc49
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2019-07-17 21:06:56 -0700

    Upgrade Google Test to 1.8.1 (#542)
    * Upgrade Google Test to 1.8.1
    
    * Update packages.mk
    
    * Update packages.mk
    
    * Update packages.mk

--- scripts/packages.mk
+++ scripts/packages.mk
@@ -5,14 +5,14 @@
 
 # rules for gtest
 ${CACHE_PREFIX}/include/gtest:
-	rm -rf gtest release-1.7.0.zip
-	wget https://github.com/google/googletest/archive/release-1.7.0.zip
-	unzip release-1.7.0.zip
-	mv googletest-release-1.7.0 gtest
-	cd gtest; $(CXX) $(CXXFLAGS) -I. -Iinclude -pthread -c src/gtest-all.cc -o gtest-all.o; cd ..
+	rm -rf gtest release-1.8.1.zip
+	wget https://github.com/google/googletest/archive/release-1.8.1.zip
+	unzip release-1.8.1.zip
+	mv googletest-release-1.8.1 gtest
+	cd gtest; $(CXX) $(CXXFLAGS) -Igoogletest -Igoogletest/include -pthread -c googletest/src/gtest-all.cc -o gtest-all.o; cd ..
 	$(AR) -rv libgtest.a gtest/gtest-all.o
 	mkdir -p ${CACHE_PREFIX}/include ${CACHE_PREFIX}/lib
-	cp -r gtest/include/gtest ${CACHE_PREFIX}/include
+	cp -r gtest/googletest/include/gtest ${CACHE_PREFIX}/include
 	mv libgtest.a ${CACHE_PREFIX}/lib
 	rm -rf release-1.7.0.zip
 
