a8babcdb81e981df9ab5574bb0360f5f6619c99d
Philip Cho <chohyu01@cs.washington.edu>
2020-01-07 14:45:27 -0800

    [CI] Fix Travis OSX test caused by clashing Python version

--- scripts/travis/travis_osx_install.sh
+++ scripts/travis/travis_osx_install.sh
@@ -6,4 +6,8 @@ if [ ${TRAVIS_OS_NAME} != "osx" ]; then
     exit 0
 fi
 
+# Prevent clash between Python 2 and 3
+brew unlink python@2
+brew link --overwrite python
+
 python3 -m pip install --upgrade pip
