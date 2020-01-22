f6f63127ea933e869855e487765b7e7a2044f099
Rong Ou <rong.ou@gmail.com>
2020-01-22 09:41:42 -0800

    Upgrade Google Test to 1.10.0 (#590)
    Thanks @rongou @hcho3 @terrytangyuan, this is now merged.

--- scripts/packages.mk
+++ scripts/packages.mk
@@ -5,16 +5,16 @@
 
 # rules for gtest
 ${CACHE_PREFIX}/include/gtest:
-	rm -rf gtest release-1.8.1.zip
-	wget https://github.com/google/googletest/archive/release-1.8.1.zip
-	unzip release-1.8.1.zip
-	mv googletest-release-1.8.1 gtest
+	rm -rf gtest release-1.10.0.zip
+	wget https://github.com/google/googletest/archive/release-1.10.0.zip
+	unzip release-1.10.0.zip
+	mv googletest-release-1.10.0 gtest
 	cd gtest; $(CXX) $(CXXFLAGS) -Igoogletest -Igoogletest/include -pthread -c googletest/src/gtest-all.cc -o gtest-all.o; cd ..
 	$(AR) -rv libgtest.a gtest/gtest-all.o
 	mkdir -p ${CACHE_PREFIX}/include ${CACHE_PREFIX}/lib
 	cp -r gtest/googletest/include/gtest ${CACHE_PREFIX}/include
 	mv libgtest.a ${CACHE_PREFIX}/lib
-	rm -rf release-1.7.0.zip
+	rm -rf release-1.10.0.zip
 
 gtest: | ${CACHE_PREFIX}/include/gtest
 
