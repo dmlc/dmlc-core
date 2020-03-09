3a2d3463673cf87442e1076b919389a2494e0b1e
Leonard Lausen <lausen@amazon.com>
2020-03-09 23:52:03 +0000

    Add -std=c++11 when compiling googletest

--- scripts/packages.mk
+++ scripts/packages.mk
@@ -9,7 +9,7 @@ ${CACHE_PREFIX}/include/gtest:
 	wget https://github.com/google/googletest/archive/release-1.10.0.zip
 	unzip release-1.10.0.zip
 	mv googletest-release-1.10.0 gtest
-	cd gtest; $(CXX) $(CXXFLAGS) -Igoogletest -Igoogletest/include -pthread -c googletest/src/gtest-all.cc -o gtest-all.o; cd ..
+	cd gtest; $(CXX) $(CXXFLAGS) -std=c++11 -Igoogletest -Igoogletest/include -pthread -c googletest/src/gtest-all.cc -o gtest-all.o; cd ..
 	$(AR) -rv libgtest.a gtest/gtest-all.o
 	mkdir -p ${CACHE_PREFIX}/include ${CACHE_PREFIX}/lib
 	cp -r gtest/googletest/include/gtest ${CACHE_PREFIX}/include
