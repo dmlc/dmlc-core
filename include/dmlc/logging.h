86dad6569f777c2151b3f1dab08838a97ae91770
Hyunsu Cho <chohyu01@cs.washington.edu>
2021-01-04 17:30:42 -0800

    Don't compile ThreadLocal() on MinGW

--- include/dmlc/logging.h
+++ include/dmlc/logging.h
@@ -435,27 +435,29 @@ class LogMessageFatal {
 #endif
       return dmlc::Error(log_stream.str());
     }
+    // Due to a bug in 32-bit MinGW, objects with non-trivial destructor cannot be thread-local.
+    // See https://sourceforge.net/p/mingw-w64/bugs/527/
+    // Hence, don't use thread-local for the log stream if the compiler is 32-bit MinGW.
+#if !(defined(__MINGW32__) || !defined(__MINGW64__))
     DMLC_NO_INLINE static Entry& ThreadLocal() {
       static thread_local Entry result;
       return result;
     }
+#endif
   };
   LogMessageFatal(const LogMessageFatal &);
   void operator=(const LogMessageFatal &);
 
-  DMLC_NO_INLINE Entry& GetEntry() {
-    // Due to a bug in 32-bit MinGW, objects with non-trivial destructor cannot be thread-local.
-    // See https://sourceforge.net/p/mingw-w64/bugs/527/
-    // Hence, don't use thread-local for the log stream if the compiler is 32-bit MinGW.
 #if defined(__MINGW32__) && !defined(__MINGW64__)
+  DMLC_NO_INLINE Entry& GetEntry() {
     return entry_;
-#else
-    return Entry::ThreadLocal();
-#endif
   }
 
-#if defined(__MINGW32__) && !defined(__MINGW64__)
   Entry entry_;
+#else
+  DMLC_NO_INLINE Entry& GetEntry() {
+    return Entry::ThreadLocal();
+  }
 #endif
 };
 #endif
