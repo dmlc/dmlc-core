4cabfeafb4f8f8e574de89cea0780c993c20c2d8
Alexander Pivovarov <apivovarov@gmail.com>
2021-03-18 17:38:52 -0700

    Allow GNU Make user to opt out of using execinfo.h via flag DMLC_LOG_STACK_TRACE (#645)
    * Enable LOG_STACK_TRACE only if EXECINFO_H_PRESENT
    
    * Allow user to explicitly opt out of execinfo.h via DMLC_LOG_STACK_TRACE
    
    * Better macro logic
    
    Co-authored-by: Hyunsu Cho <chohyu01@cs.washington.edu>

--- include/dmlc/build_config_default.h
+++ include/dmlc/build_config_default.h
@@ -22,13 +22,19 @@
      && !(defined __MINGW64__) && !(defined __ANDROID__))\
      && !defined(__CYGWIN__) && !defined(__EMSCRIPTEN__)\
      && !defined(__RISCV__) && !defined(__hexagon__)
-  #ifndef DMLC_LOG_STACK_TRACE
-  #define DMLC_LOG_STACK_TRACE 1
+  #if !defined(DMLC_LOG_STACK_TRACE)
+    #define DMLC_LOG_STACK_TRACE 1
+    #define DMLC_EXECINFO_H <execinfo.h>
+  #else
+    #if DMLC_LOG_STACK_TRACE
+      #define DMLC_EXECINFO_H <execinfo.h>
+    #else
+      #define DMLC_EXECINFO_H
+    #endif
   #endif
   #ifndef DMLC_LOG_STACK_TRACE_SIZE
   #define DMLC_LOG_STACK_TRACE_SIZE 10
   #endif
-  #define DMLC_EXECINFO_H <execinfo.h>
 #endif
 
 /* default logic for detecting existence of nanosleep() */
