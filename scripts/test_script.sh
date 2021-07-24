490093a6503b52b68e9f98b85140db1cfac2a004
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2021-07-23 21:06:04 -0700

    [CI] Fix formatting to pass lint check (#656)
    * [CI] Fix formatting to pass lint check
    
    * Attempt to fix CI for MacOS
    
    * Use Homebrew GCC when building standalone gtests

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -28,8 +28,8 @@ if [[ ${TASK} == "unittest_gtest" ]]; then
     else
         echo "USE_S3=0" >> config.mk
         echo "USE_OPENMP=1" >> config.mk
-        echo "export CXX=g++-8" >> config.mk
-        export CXX=g++-8
+        echo "export CXX=g++-11" >> config.mk
+        export CXX=g++-11
     fi
     make -f scripts/packages.mk gtest
     echo "GTEST_PATH="/tmp/gtest >> config.mk
