4ee3ad67b635326ff6fc5a73426d01f43f8d1a75
Chengyang Gu <bridgream@live.com>
2021-07-26 15:21:21 -0400

    add parquet_parser_test to TEST

--- test/dmlc_test.mk
+++ test/dmlc_test.mk
@@ -3,7 +3,7 @@ TEST=test/filesys_test test/dataiter_test\
 	test/stream_read_test test/split_test test/libsvm_parser_test\
 	test/libfm_parser_test test/split_repeat_read_test test/strtonum_test\
 	test/logging_test test/parameter_test test/registry_test\
-	test/csv_parser_test
+	test/csv_parser_test test/parquet_parser_test
 
 test/filesys_test: test/filesys_test.cc src/io/*.h libdmlc.a
 test/dataiter_test: test/dataiter_test.cc  libdmlc.a
